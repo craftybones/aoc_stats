249b0e8f12010a0bedf7f4e4426f5c373bcd7984
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2020-04-19 20:21:37 +0530

    Some npm tasks

--- src/generateIndex.js
+++ src/generateIndex.js
@@ -9,14 +9,18 @@ const spec = JSON.parse(fs.readFileSync('./spec/spec.json', 'utf8'));
 const toSVG = async (spec, data, template) => {
   spec.data.values = data;
   delete spec.data.url;
+  console.log('Compiling vega lite specs to vega...');
   const compiledSpec = vegaLite.compile(spec);
+  console.log('Parsing Vega spec...');
   const parsedSpec = vega.parse(compiledSpec.spec);
+  console.log('Creating Vega view...');
   const view = new vega.View(parsedSpec, { renderer: 'none' });
+  console.log('Rendering svg...');
   const svg = await view.toSVG();
   return template.replace('__TIMELINE__', svg);
 };
 
 toSVG(spec, data, template).then(newTemplate => {
-  fs.mkdirSync('./public');
+  if (!fs.existsSync('./public')) fs.mkdirSync('./public');
   fs.writeFileSync('./public/index.html', newTemplate, 'utf8');
 });
