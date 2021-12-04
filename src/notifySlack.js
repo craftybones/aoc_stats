bc9b475d4be22aaee877c491d0d107502a21a04a
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2021-12-04 16:00:01 +0530

    Separating Slack and Mattermost formatting

--- src/notifySlack.js
+++ src/notifySlack.js
@@ -15,28 +15,37 @@ const notify = (text, url) => {
     method: 'POST',
     headers: {'Content-Type': "application/json"}
   };
-  const data = { text, username: "aoc_bot", icon_url: "https://robohash.org/aoc_bot"  };
-  console.log(data);
+  console.log(text);
   const req = https.request(url, options, res => {
-    console.log('Status code from slack', res.statusCode);
+    console.log('Status code: ', res.statusCode);
   });
-  req.write(JSON.stringify(data));
+  req.write(text);
   req.end();
 }
 
 const notifySlack = events => {
   const text = events.map(message).join('\n');
+  const data = {
+    text,
+    username: "aoc_bot",
+    icon_url: "https://robohash.org/aoc_bot"
+  };
   const url = process.env.SLACK_URL;
-  notify(text, url);
+  notify(JSON.stringify(data), url);
 };
 
 const notifyMattermost = events => {
   const text = events.map(message).join('\n');
+  const data = {
+    text,
+    username: "aoc_bot",
+    icon_url: "https://robohash.org/aoc_bot"
+  };
   const url = process.env.MATTERMOST_URL;
-  notify(text, url);
+  notify(JSON.stringify(data), url);
 };
 
 const minutes = +process.env.MINUTES || 15;
 const filteredEvents = getEventsWithinWindow(data, minutes);
-// if (filteredEvents.length > 0) notifySlack(filteredEvents);
-if (filteredEvents.length > 0) notifyMattermost(filteredEvents);
+if (filteredEvents.length > 0 && process.env.SLACK_URL) notifySlack(filteredEvents);
+if (filteredEvents.length > 0 && process.env.MATTERMOST_URL) notifyMattermost(filteredEvents);
